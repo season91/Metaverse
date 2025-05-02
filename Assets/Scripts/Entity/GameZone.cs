db6d9648882d6e29083b9ea32d939fdb32b0c8f8
season91 <choayoung91@naver.com>
2025-05-02 12:21:36 +0900

    [feat] Game NPC 추가

---
+++ Assets/Scripts/Entity/GameZone.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameZone : MonoBehaviour
+{
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Debug.Log("Player Enter");
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Debug.Log("Player Exit");
+        }
+    }
+}
