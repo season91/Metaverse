4d0f9c34ca30c58a9696e3ac72ad5a0838b7adc7
season91 <choayoung91@naver.com>
2025-05-06 16:22:17 +0900

    [fix] Flappy Backgroud 수정

--- Assets/01.Scripts/FlappyPlane/BackGroundLopper.cs
+++ Assets/01.Scripts/FlappyPlane/BackGroundLopper.cs
@@ -22,17 +22,6 @@ public class BackGroundLopper : MonoBehaviour
     // 충돌 대상 기준으로 이어 붙여주는 로직
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("BackGround")) // 배경을 뒤로 이어 붙여줌
-        {
-            float widthOfBgObject = ((BoxCollider2D)collision).size.x;
-            Vector3 pos = collision.transform.position; //충돌한애 위치 
-
-            pos.x += widthOfBgObject * numBgCount;
-            collision.transform.position = pos;
-
-            return;
-        }
-
         // 장애물이면 장애물 배치
         Obstacle obstacle = collision.GetComponent<Obstacle>();
         if (obstacle) // null이 아니라면 장애물이므로 랜덤 배치 다시 해줄 것
